9e8be90ccdc5ad2da13f99d979e80e4ad5698d55
jhaasdijk <jhaasdijk@protonmail.com>
2018-01-10 12:56:37 +0100

    Update coins datastructure and add value information

--- money.py
+++ money.py
@@ -18,11 +18,17 @@ __author__ = "Jasper Haasdijk"
 __version__ = "0.0.1"
 __status__ = "Alpha"
 
-coins = {'bitcoin': 14446.100, 'ethereum': 719.441, 'bitcoin-cash': 2609.576, 'iota': 3.412, 'dash': 1101.123,
-         'monero': 373.981, 'qtum': 53.074, 'stellar': 0.201844, 'zcash': 508.914, 'raiblocks': 10.7311,
-         'omisego': 13.495, 'waves': 12.353, 'populous': 30.229, 'salt': 13.756, 'decred': 88.273,
-         'maidsafecoin': 0.920078}
-
+"""
+List elements consist of ('coin_id', value_when_invested, investment)
+Therefore ('bitcoin', 14446.100, 50) means that you invested in bitcoin when the price was 14446.100 with 50 dollars
+"""
+coins = [('bitcoin', 14446.100, 50), ('ethereum', 719.441, 50),
+         ('bitcoin-cash', 2609.576, 50), ('iota', 3.412, 100),
+         ('dash', 1101.123, 30), ('monero', 373.981, 100), ('qtum', 53.074, 50),
+         ('stellar', 0.201844, 50), ('zcash', 508.914, 30),
+         ('raiblocks', 10.7311, 100), ('omisego', 13.495, 30),
+         ('waves', 12.353, 20), ('populous', 30.229, 30), ('salt', 13.756, 20),
+         ('decred', 88.273, 30), ('maidsafecoin', 0.920078, 40)]
 
 def parse_args():
     parser = argparse.ArgumentParser()
