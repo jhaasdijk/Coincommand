6757ebf8504945ada898eea7c54a4f969c73c40b
jhaasdijk <jhaasdijk@protonmail.com>
2018-01-10 13:02:16 +0100

    Update coins datastructure change + update total investment to 1000 USD

--- money.py
+++ money.py
@@ -23,12 +23,13 @@ List elements consist of ('coin_id', value_when_invested, investment)
 Therefore ('bitcoin', 14446.100, 50) means that you invested in bitcoin when the price was 14446.100 with 50 dollars
 """
 coins = [('bitcoin', 14446.100, 50), ('ethereum', 719.441, 50),
-         ('bitcoin-cash', 2609.576, 50), ('iota', 3.412, 100),
+         ('bitcoin-cash', 2609.576, 50), ('iota', 3.412, 200),
          ('dash', 1101.123, 30), ('monero', 373.981, 100), ('qtum', 53.074, 50),
-         ('stellar', 0.201844, 50), ('zcash', 508.914, 30),
-         ('raiblocks', 10.7311, 100), ('omisego', 13.495, 30),
-         ('waves', 12.353, 20), ('populous', 30.229, 30), ('salt', 13.756, 20),
-         ('decred', 88.273, 30), ('maidsafecoin', 0.920078, 40)]
+         ('stellar', 0.201844, 50), ('zcash', 508.914, 50),
+         ('raiblocks', 10.7311, 100), ('omisego', 13.495, 50),
+         ('waves', 12.353, 50), ('populous', 30.229, 50), ('salt', 13.756, 40),
+         ('decred', 88.273, 30), ('maidsafecoin', 0.920078, 50)]
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -58,10 +59,9 @@ def parse_response(response):
 def check_investment(data):
     investment, total = 1000, 0
     for key in data:
-        for name, value in coins.items():
+        for name, value, investment in coins:
             if key['id'] == name:
-                total += (float(key['price_usd']) / value) * \
-                    (investment / len(coins))
+                total += (float(key['price_usd']) / value) * investment
     return total
 
 
