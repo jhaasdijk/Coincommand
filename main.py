8e05fb5e0f33efe98d336a4c8cf32561055b8e13
jhaasdijk <jhaasdijk@protonmail.com>
2018-01-12 20:31:46 +0100

    Add colours to make the displayed statistics nicer on the eyes

--- main.py
+++ main.py
@@ -72,15 +72,31 @@ def display_information(data, top=10):
     headers = ["Rank", "Coin",
                "Price (USD)", "Market Cap (USD)", "Change (24H)", "Change (7D)"]
     table = []
+
     for i, key in enumerate(data):
+        row = []
         if i == top:
             break
-        table.append((i + 1,
-                      key['symbol'],
-                      key['price_usd'],
-                      key['market_cap_usd'],
-                      "{}%".format(key['percent_change_24h']),
-                      "{}%".format(key['percent_change_7d'])))
+        row.append(i + 1)
+        row.append(key['symbol'])
+        row.append(key['price_usd'])
+        row.append(key['market_cap_usd'])
+
+        if float(key['percent_change_24h']) < 0:
+            row.append("\033[1;31;40m{}%\033[0;37;40m"
+                       .format(key['percent_change_24h']))
+        else:
+            row.append("\033[1;32;40m{}%\033[0;37;40m"
+                       .format(key['percent_change_24h']))
+
+        if float(key['percent_change_7d']) < 0:
+            row.append("\033[1;31;40m{}%\033[0;37;40m"
+                       .format(key['percent_change_7d']))
+        else:
+            row.append("\033[1;32;40m{}%\033[0;37;40m"
+                       .format(key['percent_change_7d']))
+
+        table.append(row)
 
     output = (tabulate(table, headers, floatfmt=(".0f", ".0f", ".4f",
                                                  ".0f", ".2f", ".2f"), tablefmt="fancy_grid"))
